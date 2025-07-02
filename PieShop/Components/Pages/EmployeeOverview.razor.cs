a5cbd1e08502d471b307b8800f82cd92c66b47ee
Julia Hicks <10227211@id.ohio.gov>
2025-07-02 12:38:29 -0400

    Add project files.

---
+++ PieShop/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,15 @@
+﻿using PieShop.Services;
+using PieShopHRM.Shared.Domain;
+
+namespace PieShop.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employees = MockDataService.Employees;
+        }
+    }
+}
