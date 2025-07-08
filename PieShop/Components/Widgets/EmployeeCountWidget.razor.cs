a7d3244c03d56df329b835af5e468b4cae102003
Julia Hicks <10227211@id.ohio.gov>
2025-07-08 13:15:04 -0400

    Added Dynamic component to home page and widget components

---
+++ PieShop/Components/Widgets/EmployeeCountWidget.razor.cs
@@ -0,0 +1,14 @@
+﻿using PieShop.Services;
+
+namespace PieShop.Components.Widgets
+{
+    public partial class EmployeeCountWidget
+    {
+        public int EmployeeCounter { get; set; }
+
+        protected override void OnInitialized()
+        {
+            EmployeeCounter = MockDataService.Employees.Count;
+        }
+    }
+}
