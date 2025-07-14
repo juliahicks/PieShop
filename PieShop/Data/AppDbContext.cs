12e809af52803b593a976151328f055cc8f979fb
Julia Hicks <10227211@id.ohio.gov>
2025-07-14 15:26:41 -0400

    Adding Migrations Ran the commands below: add-migration InitialMigration and update-database (created the database locally)

---
+++ PieShop/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PieShopHRM.Shared.Domain;
+
+namespace PieShop.Data
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<JobCategory> JobCategories { get; set; }
+    }
+}
--- PieShop/Program.cs
+++ PieShop/Program.cs
@@ -1,19 +1,26 @@
 using PieShop.Components;
+using PieShop.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 builder.Services
-    .AddRazorComponents()
-    .AddInteractiveServerComponents(); // This line is necessary for Blazor Server components
+   .AddRazorComponents()
+   .AddInteractiveServerComponents(); // This line is necessary for Blazor Server components  
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+       options.UseSqlServer(
+           builder.Configuration["ConnectionStrings:DefaultConnection"]
+           ));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
     app.UseHsts();
 }
 
@@ -23,7 +30,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
+   .AddInteractiveServerRenderMode();
 
 app.Run();
