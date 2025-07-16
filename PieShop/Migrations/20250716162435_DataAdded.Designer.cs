37f96fe581556ab8f87c861ab98814853220c80e
Julia Hicks <10227211@id.ohio.gov>
2025-07-16 14:29:17 -0400

    Using OnModelCreating method to add seeding data to database

--- PieShop/Migrations/20250716162435_DataAdded.Designer.cs
+++ PieShop/Migrations/20250716162435_DataAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PieShop.Data;
 
@@ -11,9 +12,11 @@ using PieShop.Data;
 namespace PieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250716162435_DataAdded")]
+    partial class DataAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
